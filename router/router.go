8bd48c7b56515d6217a799884dc5139c5b8953fe
lawrence segun <lawrencesegun025@gmail.com>
2023-10-20 20:15:51 +0100

    origin

--- router/router.go
+++ router/router.go
@@ -11,7 +11,7 @@ import (
 func Run() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"https://vought-esport.vercel.app"},
+		AllowOrigins: []string{"https://adorable-pie-9e504c.netlify.app"},
 		AllowMethods: []string{"POST", "PUT", "GET", "DELETE"},
 		AllowHeaders: []string{"Content-Type"},
 	}))
